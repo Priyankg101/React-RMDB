1b20032a7d3793942480c8072600b25c40c92343
priyankg101 <priyankg101@gmail.com>
2023-02-05 23:10:03 +0530

    fixed warnings

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,6 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-//config
-import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
-//components
-import Grid from './Grid';
-import Spinner from './Spinner';
-//Hook
-import { useMovieFetch } from '../hooks/useMovieFetch';
-//Image
-import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
-
-    const { movieId } = useParams();
-    const { state: movie, loading, error } = useMovieFetch(movieId);
-
     return (
         <>
            <div>Movie</div> 
